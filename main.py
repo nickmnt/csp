e97ad006a6f771e42f091b095b4c0c5ef5d12703
nimamt <mtaghavinima@gmail.com>
2021-12-11 18:33:25 +0330

    Code for receiving input

---
+++ main.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    rows = int(input())
+    cols = int(input())
+
+    row_vals = [0] * rows
+    row_nvals = [0] * rows
+    col_vals = [0] * cols
+    col_nvals = [0] * cols
+    for i in range(0, rows):
+        row_vals[i] = int(input())
+    for i in range(0, rows):
+        row_nvals[i] = int(input())
+    for i in range(0, cols):
+        col_vals[i] = int(input())
+    for i in range(0, cols):
+        col_nvals[i] = int(input())
+
+    
+
